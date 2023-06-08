6b38d5306dec11c62eaaa7ddddb22d8eb6a08ee8
misha-pavlov <m.pavlov@apiko.com>
2023-06-08 14:00:55 -0500

    feat(base): add sign-up

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,11 +1,9 @@
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Text } from "native-base";
-import { useRouter } from "expo-router";
 import { useAuth } from "../../context/auth";
 
-const SignIn = () => {
+const SignUp = () => {
   const { signIn } = useAuth();
-  const router = useRouter();
   console.log("sign in ()");
   return (
     <SafeAreaView
@@ -17,14 +15,9 @@ const SignIn = () => {
         borderColor: "red",
       }}
     >
-      <Text color="blue.100" onPress={() => signIn()}>
-        Sign In
-      </Text>
-      <Text color="red.100" onPress={() => router.push("/sign-up")}>
-        Sign 123
-      </Text>
+      <Text color="black" onPress={() => signIn()}>SignUp</Text>
     </SafeAreaView>
   );
 };
 
-export default SignIn;
+export default SignUp;
